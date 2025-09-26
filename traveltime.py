58168081c3ef20c711678073ae76ab37fd19be1d
SoupBorne <cdouglas92004@gmail.com>
2025-09-26 12:11:00 -0500

    initial commit

---
+++ traveltime.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import googlemaps
+from datetime import datetime
+
+def configure():
+    load_dotenv()
+
+def find_dist():
+    morning = datetime(2025, 9, 29, 7, 30)
+    apikey = os.getenv('api_key')
+    gmaps = googlemaps.Client(key=apikey)
+    distance = gmaps.distance_matrix('301 Sparkman Dr NW, Huntsville, AL 35899' , '4122 Rideout Rd, Huntsville, AL 35808' , mode='driving', units='imperial', departure_time=morning)
+    print(distance)
+
+def main():
+    configure()
+    find_dist()
+
+main()
