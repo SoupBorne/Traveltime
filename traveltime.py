ec8279fb15be9e7b451a8f8282c43a1d028c1f85
SoupBorne <cdouglas92004@gmail.com>
2025-09-26 12:25:46 -0500

    Added Comments

--- traveltime.py
+++ traveltime.py
@@ -3,9 +3,11 @@ import os
 import googlemaps
 from datetime import datetime
 
+#loads environment file
 def configure():
     load_dotenv()
 
+#Grabs apikey from .env file and uses google distance matrix to find travel time
 def find_dist():
     morning = datetime(2025, 9, 29, 7, 30)
     apikey = os.getenv('api_key')
